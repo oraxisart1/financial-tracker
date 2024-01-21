cecfb0ed46011c0e7da43c3fdff8f649d5920d5b
oraxisart <oraxisdev@gmail.com>
2024-01-21 17:21:33 +0600

    button component

---
+++ resources/js/Components/UI/Buttons/PrimaryButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    },
+});
+const emit = defineEmits(["click"]);
+
+const baseClasses =
+    "tw-text-white tw-text-3xl tw-font-semibold tw-rounded-lg tw-shadow-lg tw-py-1.5 tw-px-4";
+
+const stateClasses = computed(() =>
+    props.active ? "tw-bg-navigation" : "tw-bg-navigation-inactive",
+);
+</script>
+
+<template>
+    <button
+        :class="`${baseClasses} ${stateClasses}`"
+        @click="emit('click', $event)"
+    >
+        <slot />
+    </button>
+</template>
+
+<style scoped></style>
