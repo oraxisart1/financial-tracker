66bb9b8f9b554dfdea1c8170f1dc80e2515ebfd0
oraxisart <oraxisdev@gmail.com>
2024-02-04 11:21:15 +0600

    fix: updating account balance when transaction amount updated

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionType;
+use DB;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Account extends Model
 {
@@ -26,5 +29,23 @@ class Account extends Model
     public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function addTransaction(Transaction $transaction)
+    {
+        DB::transaction(function () use ($transaction) {
+            $this->transactions()->save($transaction);
+
+            $multiplier = $transaction->type === TransactionType::INCOME ? 1 : -1;
+            $amount = $multiplier * $transaction->amount;
+            $this->update([
+                'balance' => $this->balance + $amount,
+            ]);
+        });
     }
 }
