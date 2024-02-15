5d5b7b7877a3746318d0dbba84879d030b37ca4e
oraxisart <oraxisdev@gmail.com>
2024-02-15 16:51:57 +0600

    Transaction unit test fix

--- tests/Unit/Models/TransactionTest.php
+++ tests/Unit/Models/TransactionTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit\Models;
 
+use App\Enums\CategoryType;
 use App\Enums\TransactionType;
+use App\Models\Category;
 use App\Models\Transaction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -13,10 +15,12 @@ class TransactionTest extends TestCase
 
     public function test_type_scope_returns_filtered_transactions(): void
     {
-        $incomes = Transaction::factory(3)->create(['type' => TransactionType::INCOME]);
-        $expenses = Transaction::factory(5)->create(['type' => TransactionType::EXPENSE]);
+        $incomeCategory = Category::factory()->create(['type' => CategoryType::INCOME]);
+        $expenseCategory = Category::factory()->create(['type' => CategoryType::EXPENSE]);
+        $incomeCategory->transactions()->saveMany(Transaction::factory(3)->create());
+        $expenseCategory->transactions()->saveMany(Transaction::factory(5)->create());
 
-        self::assertCount(3, Transaction::ofType(TransactionType::INCOME)->get());
-        self::assertCount(5, Transaction::ofType(TransactionType::EXPENSE)->get());
+        self::assertCount(3, Transaction::ofType(CategoryType::INCOME)->get());
+        self::assertCount(5, Transaction::ofType(CategoryType::EXPENSE)->get());
     }
 }
